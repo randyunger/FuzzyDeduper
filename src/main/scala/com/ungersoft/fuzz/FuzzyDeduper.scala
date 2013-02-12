58d9c1fec716cb7298deb8b08557e728259a3177
Randy <randyu@skechers.com>
2013-02-12 08:45:14 -0800

    simple score

--- src/main/scala/com/ungersoft/fuzz/FuzzyDeduper.scala
+++ src/main/scala/com/ungersoft/fuzz/FuzzyDeduper.scala
@@ -2,8 +2,6 @@ package com.ungersoft.fuzz
 
 import scalaz._
 import Scalaz._
-import annotation.tailrec
-import collection.mutable
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,32 +10,76 @@ import collection.mutable
  * Time: 7:58 PM
  */
 
-class FuzzyDeduper(sentenceList: Set[String], smallestPhonemeSize: Int = 5) {
+//Use Iterable for .sliding
+class FuzzyDeduper(sentenceList: Iterable[String], smallestPhonemeSize: Int = 5) {
 
-//  val phonemeList = sentenceList map (sentence => PhonemeList(sentence, smallestPhonemeSize).phonemes)
-  val phonemesToSentences: Map[String, Set[String]] = mutable.Map.empty
+  //map each input string into a Sentence containing a list of its phonemes
+//  val sentenceList:Iterable[String] = List("a first sentence", "a second sentence", "a first another second")
+
+  lazy val processedSentences: Iterable[(String, Sentence)] =
+    sentenceList map (sentenceString => (sentenceString -> Sentence(sentenceString, smallestPhonemeSize)))
+
+  //Invert pairings (phonemes to sentences instead of sentences to phonemes)
+  lazy val phonemesSentencePairs: Iterable[(String, Sentence)] = for {
+    (fullSentence, processedSentence) <- processedSentences
+    phoneme <- processedSentence.phonemeList.phonemes
+  } yield phoneme -> processedSentence
+
+//  lazy val allPhonemes = phonemesSentencePairs map(_._1) //{ case (key, value) => key}
+
+  //Convert list to map keyed off of phonemes, and remove phonemes that appear only in one sentence
+  lazy val phonemesToSentencesWithDupeKey = phonemesSentencePairs groupBy (_._1) filter { case (key, valueList) => valueList.size > 1 }
+
+  //Entries now look like "subseq -> (subseq, List(Sentence(subseq)))". Map to remove the dupe subseq //: Map[String, Set[Sentence]]
+  lazy val phonemesToSentences = phonemesToSentencesWithDupeKey map { case(k,v) => k -> { v map { case(a,b) => b } }.toSet }
 
 
-
-
-  def candidatesByRank: List[String] = {
+  def candidatesByRank: Iterable[String] = {
     Nil
   }
 
-}
-case class Sentence(words: String) {
-  lazy val phonemeList = Nil
-  lazy val strippedSentence = ""
+  def sentencesThatSharePhonemesWith(sentence: Sentence): Set[Sentence] = phonemesToSentences.
+    filter { case (key, vlist) => vlist contains sentence }.      //Find all sentences that share phonemes with sentence
+    map { case (key, vlist) => key -> (vlist - sentence) }.        //Remove sentence from list
+    values.toSet.flatten
+
+  def phonemesSharedBetween(sentenceOne: Sentence, sentenceTwo: Sentence): Set[String] = phonemesToSentences.
+    filter { case (k, vlist) => vlist.contains(sentenceOne) && vlist.contains(sentenceTwo) }.
+    keys
+
+  def phonemeRarity(phoneme: String): (Int, Int) = (phonemesToSentences.get(phoneme) map (_.size), phonemesToSentences.size)
 }
 
-case class PhonemeList(sentence: String, smallestPhonemeSize: Int) {
-  lazy val phonemes = findPhonemes(Nil, smallestPhonemeSize)
+case class MatchScorer(sentenceOne: Sentence, sentenceTwo: Sentence) {
 
-  @tailrec
-  private def findPhonemes(phonemeList: List[List[String]], size: Int): List[String] = {
-    if(size == sentence.length) phonemeList
-    else findPhonemes(phonemeList :: phonemesOfSize(size), size + 1)
+}
+
+case class Sentence(fullSentence: String, smallestPhonemeSize: Int) {
+  lazy val strippedSentence = fullSentence.replaceAll(" ","")
+  lazy val phonemeList = PhonemeList(strippedSentence, smallestPhonemeSize)
+
+  def scoreVersus(thatSentence: Sentence): Long = {
+    val ourPhonemes = phonemeList.phonemes
+    val theirPhonemes = thatSentence.phonemeList.phonemes
+    val sharedPhonemes = ourPhonemes.intersect(theirPhonemes)
+
+    val ourPercent = sharedPhonemes.size.toFloat / ourPhonemes.size             //Percent of us contained in them
+    val theirPercent = sharedPhonemes.size.toFloat / theirPhonemes.size         //Percent of them contained in us
+
+    ourPercent * theirPercent
+  }
+}
+
+case class PhonemeList(words: String, smallestPhonemeSize: Int) {
+  private val largestPhonemeSize = words.length  //todo: Configurable?
+
+  lazy val phonemes: Seq[String] = findPhonemes(Seq.empty, smallestPhonemeSize)
+
+  @annotation.tailrec
+  private def findPhonemes(phonemeList: Seq[String], size: Int): Seq[String] = {
+    if(size == largestPhonemeSize) phonemeList
+    else findPhonemes(phonemeList ++ phonemesOfSize(size), size + 1)
   }
 
-  private def phonemesOfSize(n: Int) = sentence.toList.iterator.sliding(n, 1)
+  private def phonemesOfSize(n: Int):Set[String] = words.iterator.sliding(n, 1) map (_.mkString) toSet  //Iterate via a sliding window of size n
 }
--- src/test/scala/com/ungersoft/fuzz/FuzzyDeduperTest.scala
+++ src/test/scala/com/ungersoft/fuzz/FuzzyDeduperTest.scala
@@ -1,11 +1,18 @@
 package com.ungersoft.fuzz
 
+import org.specs2.mutable.Specification
+
 /**
  * Created with IntelliJ IDEA.
  * User: randy
  * Date: 2/11/13
  * Time: 9:15 AM
  */
-class FuzzyDeduperTest {
-
+class FuzzyDeduperTest extends Specification {
+  "Fuzzy Deduper" should {
+    "exist" in {
+      new FuzzyDeduper(Seq("a"), 1)
+      1 must be equalTo(1)
+    }
+  }
 }
